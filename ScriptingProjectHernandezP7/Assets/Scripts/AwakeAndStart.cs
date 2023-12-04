bc5db77078320792931df43cb5ec80e729219744
Kenneth Hernandez <724914@student.dusd.net>
2023-12-04 14:45:21 -0800

    Did The AwakeAndStart Scripting Project.
    Finished the scripting assignment added a cube and a script called Awakening

---
+++ ScriptingProjectHernandezP7/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake ()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
